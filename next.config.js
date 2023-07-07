4ca1a90e6c9d01327b9a5fede0977354ecd2cb78
nservais <nikita@lab-box.studio>
2023-07-07 13:55:10 +0200

    chore: fix appDir option to experimental

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  appDir: true,
+  experimental: {
+    appDir: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['apod.nasa.gov'],
