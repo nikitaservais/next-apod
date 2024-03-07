a91385bc4b7ca9a1ede754a92c59e20a389057eb
nservais <93200020+nikitaservais@users.noreply.github.com>
2024-03-07 16:50:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { fetchAPODs } from '../services/apod'
 import React from 'react'
 import { ApoList } from '../components/ApoList'
 
+export const revalidate = 60 * 60 * 12
+
 export default async function Home() {
   const apos = await fetchAPODs()
   return <ApoList apos={apos} />
